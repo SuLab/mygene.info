3cefa1e3f2f9f98c5860d355253168fe3b652c28
Sebastien Lelong <slelong@scripps.edu>
2016-06-30 11:35:24 -0700

    some cherry-picking from merge (mapping,exons list)

--- src/dataload/sources/ucsc/ucsc_base.py
+++ src/dataload/sources/ucsc/ucsc_base.py
@@ -45,7 +45,7 @@ def load_exons_for_species(species, exons_key='exons'):
             if geneid not in gene2exons:
                 gene2exons[geneid] = {exons_key: ref2exons[refseq]}
             else:
-                gene2exons[geneid][exons_key] = ref2exons[refseq]
+                gene2exons[geneid][exons_key].extend(ref2exons[refseq])
 
     load_done('[%d, %s]' % (len(gene2exons), timesofar(t0)))
 
